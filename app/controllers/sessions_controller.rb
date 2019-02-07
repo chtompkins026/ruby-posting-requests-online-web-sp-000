a88474a903a8a6e0d3d1c450dd205992c69e337b
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-07 16:21:02 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     resp = Faraday.get("https://foursquare.com/oauth2/access_token") do |req|
-      req.params['client_id'] = ENV['FOURSQUARE_CLIENT_ID']
+      req.params['client_id'] = 'W3ZNT3VH1FWIA21O4JRG204J1DMHOABWPDHLAUXD5XFVV23W'
       req.params['client_secret'] = ENV['FOURSQUARE_SECRET']
       req.params['grant_type'] = 'authorization_code'
       req.params['redirect_uri'] = "http://localhost:3000/auth"
